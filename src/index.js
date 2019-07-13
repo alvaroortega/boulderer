3177d7d000d8d6299b4beec34544425ce7abd881
alvaroortega <alvaro.ortega@fieldaware.com>
2019-07-13 19:44:09 +0100

    Configure Webpack and babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return <div>Welcome to Boulderer</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
